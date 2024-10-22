6d97e5d8bbffa35033ea2d81979a46fa33f6c33f
luana2b <161466895+luana2b@users.noreply.github.com>
2024-10-22 10:24:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'espaço',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'O russo Yuri Gagarin (1934-1968) foi a primeira pessoa a viajar para o espaço, o que aconteceu em 12 de abril de 1961',
+)
+criaCartao(
+    'montanha',
+    'Qual a montanha mais alta do Brasil?',
+    'Pico da Neblina é o ponto mais elevado do Brasil, contando com 2995 metros de altura.',
+)
+ criaCartao(
+    'fases da lua',
+    'Quais são as fases da Lua?',
+    'As fases da lua são quatro: nova, crescente, cheia e minguante. Cada uma delas dura 7 ou 8 dias',
+)
+    criaCartao (
+        'sol',
+     'Quanto tempo demora  pra luz do sol chega ate a terra?',
+    'A luz do Sol leva cerca de 8,3 minutos para chegar até a Terra, ou seja, nossa estrela está a uma distância de 8,3 minutos-luz de nós',
+
+    )
+    
+
+ 
